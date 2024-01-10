f24c47138769473b14ebda45f0edc66f29d9af43
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2024-01-10 21:30:31 +0100

    .

--- books/views.py
+++ books/views.py
@@ -3,12 +3,6 @@ from django.views import View
 from django.views.generic import ListView
 from .models import Book
 
-# Using a function-based view
-# def book_list(request):
-#     books = Book.objects.all()
-#     return render(request, 'books.html', {'books': books})
-
-# Using a class-based view
 class BookListView(ListView):
     model = Book
     template_name = 'books.html'
